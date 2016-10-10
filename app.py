78f14575dd9f8b4a93fb27442822d5d429dcbbce
bruno <bruno.amv@gmail.com>
2016-10-10 19:47:57 -0300

    Tentativa de colocar no ar

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "<h1 align=center>Espaço Rogue (Rosa Yogue)!</h1><h4 align=center>Em construção</h4><br><br><br><h2 align=center>Acesse: https://www.facebook.com/espacorogue/ </h2> <h4 align=center>Para maiores informações</h4>"
 
 if __name__ == '__main__':
     app.run()
