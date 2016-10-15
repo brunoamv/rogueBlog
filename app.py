d0d6dd7c00212e2a8f91730080b2e158f56392b9
bruno <bruno.amv@gmail.com>
2016-10-15 20:21:49 -0300

    Mudando o static

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1 align=center>Espaço Rogue (Rosa Yogue)!</h1><h4 align=center>Em construção</h4><br><br><br><h2 align=center>Acesse: https://www.facebook.com/espacorogue/ </h2> <h4 align=center>Para maiores informações</h4>"
+    return "<h1 align=center>Espaço Rogue (Rosa Yogue)!</h1><h4 align=center>Em construção<br>Assim como nós em constante construção</h4><br><br><br><h2 align=center>Acesse: <a href=https://www.facebook.com/espacorogue/>Face Aqui</a> </h2> <h4 align=center>Para maiores informações</h4>"
 
 if __name__ == '__main__':
     app.run()
